48c77292d4966c420284cb5071e96fe44daac505
midoriko <mollyme@gmail.com>
2016-11-10 23:11:24 -0500

    dwhouse v1.0 website

---
+++ send_toemail.php
@@ -0,0 +1,153 @@
+<?php
+ 
+if(isset($_POST['email'])) {
+ 
+     
+ 
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+ 
+    $email_to = "dorsn.play@gmail.com";
+ 
+    $email_subject = ($_POST['product_service']);
+ 
+     
+ 
+     
+ 
+  /*  function died($error) {
+ 
+        // your error code can go here
+ 
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+ 
+        echo "These errors appear below.<br /><br />";
+ 
+        echo $error."<br /><br />";
+ 
+        echo "Please go back and fix these errors.<br /><br />";
+ 
+        die();
+ 
+    }
+
+     
+ 
+    // validation expected data exists
+ 
+    if(!isset($_POST['full_name']) ||
+ 
+        !isset($_POST['email']) ||
+ 
+        !isset($_POST['service']) ||
+ 
+        !isset($_POST['inquiry'])) {
+ 
+        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+ 
+    }*/
+ 
+     
+ 
+    $full_name = $_POST['full_name']; // required
+ 
+    $company = $_POST['company']; // required
+ 
+    $email_from = $_POST['email']; // required
+ 
+    $service = $_POST['service']; // not required
+ 
+    $inquiry = $_POST['inquiry']; // required
+ 
+     
+ 
+    $error_message = "";
+ 
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+ 
+  if(!preg_match($email_exp,$email_from)) {
+ 
+    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+ 
+  }
+ 
+    $string_exp = "/^[A-Za-z .'-]+$/";
+ 
+  if(!preg_match($string_exp,$full_name)) {
+ 
+    $error_message .= 'The Name you entered does not appear to be valid.<br />';
+ 
+  }
+ 
+  if(!preg_match($string_exp,$company)) {
+ 
+    $error_message .= 'The company name you entered does not appear to be valid.<br />';
+ 
+  }
+ 
+  if(strlen($inquiry) < 2) {
+ 
+    $error_message .= 'The inquiry you entered do not appear to be valid.<br />';
+ 
+  }
+ 
+  if(strlen($error_message) > 0) {
+ 
+    died($error_message);
+ 
+  }
+ 
+    $email_message = "Inquiry details below.\n\n";
+ 
+     
+ 
+    function clean_string($string) {
+ 
+      $bad = array("content-type","bcc:","to:","cc:","href");
+ 
+      return str_replace($bad,"",$string);
+ 
+    }
+ 
+     
+ 
+    $email_message .= "Full Name: ".clean_string($full_name)."\n";
+ 
+    $email_message .= "Company Name: ".clean_string($company)."\n";
+ 
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+ 
+    $email_message .= "Service / Product: ".clean_string($service_product)."\n";
+ 
+    $email_message .= "Inquiry: ".clean_string($inquiry)."\n";
+ 
+     
+ 
+     
+ 
+// create email headers
+ 
+$headers = 'From: '.$email_from."\r\n".
+ 
+'Reply-To: '.$email_from."\r\n" .
+ 
+'X-Mailer: PHP/' . phpversion();
+ 
+@mail($email_to, $email_subject, $email_message, $headers);  
+ 
+?>
+ 
+ 
+ 
+<!-- include your own success html here -->
+ 
+ 
+ 
+Thank you for contacting us. We will be in touch with you very soon.
+ 
+ 
+ 
+<?php
+ 
+}
+ 
+?>
